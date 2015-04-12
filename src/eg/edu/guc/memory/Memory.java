bcde4aad921dc94209f596a23979563732753326
kady <elkadyabdelrahman@gmail.com>
2015-04-12 14:51:36 +0200

    Creating Memory package and class

---
+++ src/eg/edu/guc/memory/Memory.java
@@ -0,0 +1,50 @@
+package eg.edu.guc.memory;
+
+/**
+ * Created by kady on 12/04/15.
+ */
+public class Memory {
+
+    private static int[] memory;
+    private static int size;
+
+    public Memory() {
+        init();
+    }
+
+    public static int loadWord(int location) {
+        //TODO implement and add documentation
+        return -1;
+    }
+
+    public static byte loadByte(int location) {
+        //TODO implement and add documentation
+        return -1;
+    }
+
+    public static byte loadByteUnsigned(int location) {
+        //TODO implement and add documentation
+        return -1;
+    }
+
+    public static boolean clear() {
+        //TODO implement and add documentation
+        return true;
+    }
+
+    public static void init() {
+        //TODO implement and add documentation
+        memory = new int[size];
+    }
+
+
+    //Getters and setters
+
+    public int[] getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int[] memory) {
+        Memory.memory = memory;
+    }
+}
