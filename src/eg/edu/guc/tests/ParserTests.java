ac8e603a296fa0ffc1a42968abdd5b9a027db122
Abkreno <mohamedelhamamsy789@gmail.com>
2015-04-18 01:51:30 +0200

    modifies parser test to test if the type of the instruction if I

--- src/eg/edu/guc/tests/ParserTests.java
+++ src/eg/edu/guc/tests/ParserTests.java
@@ -1,6 +1,11 @@
 package eg.edu.guc.tests;
 
+import eg.edu.guc.instructions.IFormatInstruction;
+import eg.edu.guc.instructions.Instruction;
+import eg.edu.guc.mips.Main;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by mohamed on 18/04/15.
@@ -9,7 +14,11 @@ public class ParserTests {
 
     @Test
     public void testingParser() {
-        
+        Main.initializeNameRegisterHtbl();
+        Main.readInstructionsData();
+        String inst = "addi $t0, $0, 10";
+        Instruction instruction = Main.parse(inst);
+        assertTrue("addi instruction should be instance of IFormatInstruction class", instruction instanceof IFormatInstruction);
     }
 
 }
