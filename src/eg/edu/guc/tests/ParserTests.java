344a5fe358988b037b3bd608a9066882b233307a
Abkreno <mohamedelhamamsy789@gmail.com>
2015-04-18 01:31:18 +0200

    Adds function for testing the parser

--- src/eg/edu/guc/tests/ParserTests.java
+++ src/eg/edu/guc/tests/ParserTests.java
@@ -1,7 +1,15 @@
 package eg.edu.guc.tests;
 
+import org.junit.Test;
+
 /**
  * Created by mohamed on 18/04/15.
  */
 public class ParserTests {
+
+    @Test
+    public void testingParser() {
+        
+    }
+
 }
