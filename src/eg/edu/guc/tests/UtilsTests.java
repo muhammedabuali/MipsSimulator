7150a4dff7dc8811e4ec7812c45565be19cb7b3a
kady <elkadyabdelrahman@gmail.com>
2015-04-22 15:43:42 +0200

    Modifying utilTests , Adding more tests

--- src/eg/edu/guc/tests/UtilsTests.java
+++ src/eg/edu/guc/tests/UtilsTests.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -20,12 +21,22 @@ public class UtilsTests {
     public void byteArrayToIntTest() {
         byte[] data = new byte[]{0x10, 0x12, 0x13, 0x14};
         assertEquals(269619988, Utilities.byteArrayToInt(data));
+
+        data = new byte[]{0x00, 0x00, 0x13, 0x14};
+        assertEquals(4884, Utilities.byteArrayToInt(data));
     }
 
     @Test
     public void intToByteArrayTest() {
         int value = 269619988;
-        assertTrue(Arrays.equals(new byte[]{0x10, 0x12, 0x13, 0x14}, Utilities.intToByteArray(value)));
+
+
+        assertTrue(Arrays.equals(new byte[]{0x10, 0x12, 0x13, 0x14}, Utilities.intToByteArray(value, 4)));
+        //FIXME WAT BigEndian ?
+        assertTrue(Arrays.equals(new byte[]{0x10, 0x12, 0x13, 0x14, 0x00}, Utilities.intToByteArray(value, 5)));
+
+        assertFalse(Arrays.equals(new byte[]{0x12, 0x12, 0x13, 0x14}, Utilities.intToByteArray(value, 4)));
+        assertFalse(Arrays.equals(new byte[]{0x10, 0x12, 0x13, 0x14}, Utilities.intToByteArray(value, 5)));
     }
 
 }
