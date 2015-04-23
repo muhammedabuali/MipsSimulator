0cdee1781335c446e32df918c54aa03c5d55dc41
Mohamed Mostafa <mohamedelhamamsy789@gmail.com>
2015-04-23 18:47:49 +0200

    Adds hashtabels

--- src/eg/edu/guc/utilities/Utils.java
+++ src/eg/edu/guc/utilities/Utils.java
@@ -1,5 +1,12 @@
 package eg.edu.guc.utilities;
 
+import java.util.Hashtable;
+
+import eg.edu.guc.registers.Register;
+
 public class Utils {
+
+	private static Hashtable<String, String[]> instructionNameData;
+	private static Hashtable<String, Register> nameRegister;
 
 }
