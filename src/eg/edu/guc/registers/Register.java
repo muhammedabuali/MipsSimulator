33aa1980ccac4500ac50c553bdcd763ec6f7d693
Mohamed Mostafa <mohamedelhamamsy789@gmail.com>
2015-04-23 21:07:31 +0200

    Adds number field to Register class

--- src/eg/edu/guc/registers/Register.java
+++ src/eg/edu/guc/registers/Register.java
@@ -1,6 +1,5 @@
 package eg.edu.guc.registers;
 
-
 /**
  * Created by kady on 12/04/15.
  * <p/>
@@ -9,25 +8,37 @@ package eg.edu.guc.registers;
  * @author kady
  */
 public class Register {
-    private int data;
+	private int data, number;
 
+	public Register(int data) {
+		this.data = data;
+	}
 
-    public Register(int data) {
-        this.data = data;
-    }
+	public Register(int data, int number) {
+		this.data = data;
+		this.number = number;
+	}
 
+	public int getData() {
+		return data;
+	}
 
-    public int getData() {
-        return data;
-    }
+	public int getNumber() {
+		return number;
+	}
 
-    public void setData(int data) {
-        //TODO Check if we can throw an exception , or remove this and depend on the final declaration's exception
-        if (this == RegisterFile.ZERO_REGISTER) {
-            System.err.println("Register ZERO can't be set");
-            return;
-        }
-        this.data = data;
-    }
+	public void setData(int data) {
+		// TODO Check if we can throw an exception , or remove this and depend
+		// on the final declaration's exception
+		if (this == RegisterFile.ZERO_REGISTER) {
+			System.err.println("Register ZERO can't be set");
+			return;
+		}
+		this.data = data;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
 
 }
