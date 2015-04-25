67bfc4ea3a48e5918065f8f1ae2cc22fbe7c8dab
alithephantom <alithephantom@gmail.com>
2015-04-25 16:06:32 +0300

    Adding new Test in Mips ... Fails !

--- src/eg/edu/guc/tests/MipsTests.java
+++ src/eg/edu/guc/tests/MipsTests.java
@@ -2,6 +2,8 @@ package eg.edu.guc.tests;
 
 import static org.junit.Assert.*;
 
+import eg.edu.guc.registers.IDEXRegister;
+import eg.edu.guc.registers.IFIDRegister;
 import org.junit.Test;
 
 import eg.edu.guc.mips.Mips;
@@ -11,44 +13,63 @@ import eg.edu.guc.utils.Utilities;
 
 public class MipsTests {
 
-	@Test
-	public void testGetRInstructionBitStream() {
-		RegisterFile.init();
-		Mips m = new Mips();
+    @Test
+    public void testGetRInstructionBitStream() {
+        RegisterFile.init();
+        Mips m = new Mips();
 
-		int instCode = m.getInstructionBitStream(Parser.parseInstruction(
-				"add $t1, $t2, $t3", 1));
-		assertTrue(
-				"The code for the instruction should be 0x014b4820 as found in mars",
-				Integer.toHexString(instCode).equals("14b4820"));
-	}
+        int instCode = m.getInstructionBitStream(Parser.parseInstruction(
+                "add $t1, $t2, $t3", 1));
+        assertTrue(
+                "The code for the instruction should be 0x014b4820 as found in mars",
+                Integer.toHexString(instCode).equals("14b4820"));
+    }
 
-	@Test
-	public void testGetIInstructionBitStream() {
-		RegisterFile.init();
-		Mips m = new Mips();
+    @Test
+    public void testGetIInstructionBitStream() {
+        RegisterFile.init();
+        Mips m = new Mips();
 
-		int instCode = m.getInstructionBitStream(Parser.parseInstruction(
-				"addi $t1, $t2, 11", 1));
-		assertTrue(
-				"The code for the instruction should be 0x2149000b as found in mars",
-				Integer.toHexString(instCode).equals("2149000b"));
-	}
+        int instCode = m.getInstructionBitStream(Parser.parseInstruction(
+                "addi $t1, $t2, 11", 1));
+        assertTrue(
+                "The code for the instruction should be 0x2149000b as found in mars",
+                Integer.toHexString(instCode).equals("2149000b"));
+    }
 
-	@Test
-	public void testGetInstructionNameByOpcodePart() {
-		RegisterFile.init();
-		int instruction = Integer.parseInt("2149000b", 16);
-		int opcode = (instruction >> 26) << 26;
+    @Test
+    public void testGetInstructionNameByOpcodePart() {
+        RegisterFile.init();
+        int instruction = Integer.parseInt("2149000b", 16);
+        int opcode = (instruction >> 26) << 26;
 
-		if (opcode == 0) {
-			// R format Instruction need to get function code
-			int functioncode = instruction & ((1 << 6) - 1); // anding with mask
-			opcode |= functioncode;
-		}
+        if (opcode == 0) {
+            // R format Instruction need to get function code
+            int functioncode = instruction & ((1 << 6) - 1); // anding with mask
+            opcode |= functioncode;
+        }
 
-		String instructionName = Utilities.getInstructionNameByOpcode(opcode);
-		assertTrue("The instruction should be addi",
-				instructionName.equals("addi"));
-	}
+        String instructionName = Utilities.getInstructionNameByOpcode(opcode);
+        assertTrue("The instruction should be addi",
+                instructionName.equals("addi"));
+    }
+
+    @Test
+    public void testAddInstructionDecode() {
+        RegisterFile.init();
+        //add t0 t1 t2
+        int instruction = Integer.parseInt("000000" + "01000" + "01001" + "01010" + "00000" + "100000", 2);
+        IFIDRegister.setInstruction(instruction);
+
+        Mips m = new Mips();
+        m.decode();
+
+        assertTrue("funct should be 100000", Utilities.getSubset(IDEXRegister.getOffset(), 0, 5) == 0b100000);
+
+        System.out.println(Utilities.getRegisterByNumber(IDEXRegister.getRt()).getNumber());
+        System.out.println(Utilities.getRegisterByNumber(IDEXRegister.getRt()));
+        System.out.println(RegisterFile.T1_REGISTER);
+        assertTrue("rt should be $t1", Utilities.getRegisterByNumber(IDEXRegister.getRt()) == RegisterFile.T1_REGISTER);
+        assertTrue("rd should be $t0", Utilities.getRegisterByNumber(IDEXRegister.getRd()).equals(RegisterFile.T0_REGISTER));
+    }
 }
